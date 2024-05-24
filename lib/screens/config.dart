0ada4c662b4d9a80cdc692faf6335da1ec9f50a6
saniyaa7 <saniyachaudhari07@gmail.com>
2024-05-24 17:33:44 +0530

    add chnages

--- lib/screens/config.dart
+++ lib/screens/config.dart
@@ -1,5 +1,5 @@
 class Config {
-  static const String apiUrl = 'http://localhost:3000/api/auth';
-  static const String notificationUrl = 'http://localhost:3036';
+  static const String apiUrl = 'http://192.168.99.189:3000/api/auth';
+  static const String notificationUrl = 'http://192.168.99.189:3036';
   // static const String apiKey = 'your_api_key_here';
 }
